dd01b30f362a382623ad3cd93f7fd50cd0e59ceb
Luiz Berto <diasbertoluiz@gmail.com>
2019-10-26 17:49:01 -0300

    Altera script de geração de entradas para gerar custos e pontuações iguais, para utilizar menos memória na solução DP

--- tests/scripts/generate_test_case.py
+++ tests/scripts/generate_test_case.py
@@ -2,10 +2,10 @@ import sys
 
 def main():
 	size = int(sys.argv[1])
-	lines = [("{} {}".format(i * 10, i)) for i in range(size, 0, -1)]
-	max_amount = size * 10
+	lines = [("{} {}".format(i, i)) for i in range(size, 0, -1)]
+	max_amount = size
 	print(str(max_amount) + " " + str(size))
-	lines[0] = "{} {}".format(size * 10, size + 1)
+	lines[0] = "{} {}".format(size, size + 1)
 	for line in lines:
 		print(line)
 
